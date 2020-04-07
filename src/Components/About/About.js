3a24be06d347b6eeb2118060eb133806a83aaf11
spiltmiruku <spiltmiruku@gmail.com>
2020-04-07 06:48:49 -0600

    Added general info page; Fixed opacity issue in header;

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './about.css';
+import React from "react";
+import "./about.css";
 
 const About = () => {
-    return (
-        <div>
-
+  return (
+    <div>
+      <section className="info-border">
+        <div className="info-box">
+          <p className="gen-info">Monday - Saturday 11:00AM - 9:00PM</p>
+          <p className="gen-info">
+            123 S Milk Tea St. Salt Lake City, UT 84111
+          </p>
+          <p className="gen-info">meo@meotea.com</p>
+          <p className="gen-info">801-666-8888</p>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default About;
