13c4fb3d04d5bfacfd45fd9ba6797be94941c5d5
spiltmiruku <spiltmiruku@gmail.com>
2020-04-06 21:15:24 -0600

    Added banner image, additional styling for header, and overlay on banner; Styled menu page with styling;

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './header.css';
+import "./header.css";
+import teawood from "../../resources/tea-wood.jpg";
 
 const Header = () => {
   return (
     <div>
-        <div className='logo'>meo TEA</div>
-      <div className='header-nav-links'>
-        <Link to="/">HOME</Link>
-      </div>
-      <div className='header-nav-links'>
-        <Link to="/menu">MENU</Link>
-      </div>
-      <div className='header-nav-links'>
-        <Link to="/about">HOURS</Link>
-      </div>
-      <div className='header-banner'>
-          <h1>premium & handcrafted milk teas</h1>
+      <section className="header-logo-links">
+        <div className="logo">meo TEA</div>
+
+        <section className="header-links-box">
+          <div className="header-nav-links">
+            <Link to="/">HOME</Link>
+          </div>
+          <div className="header-nav-links">
+            <Link to="/menu">MENU</Link>
+          </div>
+          <div className="header-nav-links">
+            <Link to="/about">HOURS</Link>
+          </div>
+        </section>
+      </section>
+
+      <div className="header-banner">
+        <img id="picture-banner" src={teawood} alt="bubble tea" />
+        <div className='banner-overlay'>
+        </div>
+        <h1 className="banner-text">premium & handcrafted milk teas</h1>
       </div>
     </div>
   );
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import './menu.css';
+import React from "react";
+import "./menu.css";
+import teapour from "../../resources/tea-pour.jpg";
 
 const Menu = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="all-products">
+        <section className="menu-border">
+          <article className="product">
+            <div className="image-wrapper">
+              <img id="product-img" src={teapour} alt="bubble tea" />
+            </div>
+            <div className="tea-info">
+              <h1 className="tea-name">Classic | Milk Tea</h1>
+              <p className="tea-description">
+                Lactose-free milk, handcrafted brown sugar syrup, and black tea
+              </p>
+            </div>
+          </article>
 
-        </div>
-    )
-}
+          <article className="product">
+            <div className="image-wrapper">
+              <img id="product-img" src={teapour} alt="bubble tea" />
+            </div>
+            <div className="tea-info">
+              <h1 className="tea-name">Best Friend | Green Milk Tea</h1>
+              <p className="tea-description">
+                Lactose-free milk, handcrafted brown sugar syrup, and green tea
+              </p>
+            </div>
+          </article>
+
+          <article className="product">
+            <div className="image-wrapper">
+              <img id="product-img" src={teapour} alt="bubble tea" />
+            </div>
+
+            <div className="tea-info">
+              <h1 className="tea-name">Thai Tea</h1>
+              <p className="tea-description">
+                Handcrafted simple syrup, loose-leaf thai tea, and lactose free
+                milk
+              </p>
+            </div>
+          </article>
+
+          <article className="product">
+            <div className="image-wrapper">
+              <img id="product-img" src={teapour} alt="bubble tea" />
+            </div>
+
+            <div className="tea-info">
+              <h1 className="tea-name">
+                Pineberry | Pineapple + Strawberry Green Tea
+              </h1>
+              <p className="tea-description">
+                A thirst quenching blend of handcrafted pineapple and strawberry
+                syrup with green tea
+              </p>
+            </div>
+          </article>
+
+          <article className="product">
+            <div className="image-wrapper">
+              <img id="product-img" src={teapour} alt="bubble tea" />
+            </div>
+
+            <div className="tea-info">
+              <h1 className="tea-name">Piglet</h1>
+              <p className="tea-description">
+                Ice cold coconut milk blended with our handcrafted strawberry
+                syrup
+              </p>
+            </div>
+          </article>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
